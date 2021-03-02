d754f98c9f27547846146146ce605ad6c7466d25
SAGURULEV <boxersergu7@yandex.ru>
2021-03-02 20:31:15 +0300

    zadacha3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,13 +1,32 @@
 "use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    let bodyLoan = amount - contribution;
-    let P = 10 / 100 / 12;
-    let n = 
-    let monthlyFee = bodyLoan / bodyLoan*(P+P/(((1+P)Math.pow(n))------1)), где: S - тело кредита, P - 1/12 процентной ставки (от 0 до 1), n - количество месяцев ^ - возведение в степень
-    // return totalAmount;
+    if (percent <= 0 || typeof(percent) === "undefined") {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
+    if (contribution < 0 || typeof(contribution) === "undefined") {
+        return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
+    }
+    if (amount <= 0 || typeof(amount) === "undefined") {
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    }
+
+    let P = (percent / 12 / 100);
+    let S = parseInt(amount) - parseInt(contribution);
+    if (day <= 0) {
+        return `Параметр "Срок ипотеки" содержит неправильное значение ${date}`;
+    }
+    let payment = S * (P + P / (((1 + P) ** n) - 1));
+    let totalAmount = (payment * n).toFixed(2);
+    return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
-}[']]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]jhhhhhhhhhhhhhh-x']eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee4u4uzzzzzzz7yu12
+    let unknown = "Аноним."
+    if (typeof(percent) === "undefined") {
+        return `Привет, Fyjyмир! Меня зовут ${unknown}`
+    } else {
+    return `Привет, мир! Меня зовут ${name}.`
+    }
+}
