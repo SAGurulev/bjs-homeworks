b9aae8e0b06069fda5aeb59dcc6decc3f19097c8
SAGURULEV <boxersergu7@yandex.ru>
2021-03-14 20:08:23 +0300

    ZadachaProto

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,25 +1,39 @@
 "use strict"
 
 String.prototype.isPalindrome = function isPalindrome () {
-
+        let stringLower = this.toLowerCase();
+        console.log(stringLower);
+        let strDelete = stringLower.split(" ").join('');
+        console.log(strDelete);
+        let strRevers = strDelete.split("").reverse().join('');
+        console.log(strRevers);
+        return strDelete === strRevers;
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    let quantityScore = marks.length;
+    let average = 0;
+
+    if (quantityScore == 0) {
+        return 0;
+    }
+    
+    for (let i = 0; i < marks.length; i++) {
+        average = average + marks[i];
+    }
+    average = average / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
     let now = (new Date).getTime();
     let date = Date.parse(birthday);
-    console.log(typeof(birthday));
-    console.log(date);
     let diff = Math.round((now - date)/(1000*60*60*24));
-    console.log(diff);
     let averageMonth = 30.44;
     let age = Math.floor(diff/(averageMonth*12));
-    console.log(age);
     if (age >= 18 ) {
         return age;
     }
