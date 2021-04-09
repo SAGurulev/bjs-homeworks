9d4d6afd7310f393a1392992ab4454854a0ee61a
SAGURULEV <boxersergu7@yandex.ru>
2021-04-09 21:37:19 +0300

    zadacha

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -39,7 +39,7 @@ function sum(...args) {
     }, 0);
   }
   function compareArrays( arr1, arr2 ) {
-    const compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+    return compareArrays = (arr1, arr2) => arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
     // return arr1.filter(function(i) {return arr2.indexOf(i) < 0;});
   }
 
